c36fcb048dcbd5b0d37fc5436b2d69d7bff9a17a
Hasan Bijoy <mrhbijoy@gmail.com>
2024-03-17 14:42:23 +0600

    added try

--- Number_Guessing_Game/ng.js
+++ Number_Guessing_Game/ng.js
@@ -1,10 +1,13 @@
 let num = Math.floor(Math.random() * 100) + 1;
 let guesses = [];
+let score = 0;
 
 document
   .getElementById("guess-form")
   .addEventListener("submit", function (event) {
     event.preventDefault();
+    document.getElementById("try").innerText =
+      "You have tried " + (score += 1) + " times.";
     let num2 = document.getElementById("guess").value;
     if (guesses.includes(num2)) {
       document.getElementById("message").innerText =
@@ -17,13 +20,17 @@ document
         document.getElementById("message").innerText = "Too high, try again";
       } else if (num2 < num) {
         document.getElementById("message").innerText = "Too low, try again";
-      } else {
+      } else if (num2 == num) {
         document.getElementById("message").innerText =
           "Congratulations! You guessed the number";
+      } else {
+        score -= 1;
+        document.getElementById("message").innerText = "Invalid input";
       }
     }
   });
 
 document.getElementById("revealNumber").addEventListener("click", function () {
   alert("The secret number is " + num);
+  score += 10;
 });
