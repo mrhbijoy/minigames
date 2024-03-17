b400fc13adc9d750bc38e143c2f1de8219d16dcd
Hasan Bijoy <mrhbijoy@gmail.com>
2024-03-17 16:11:29 +0600

    Refactor number guessing game logic and update game title

--- Number_Guessing_Game/ng.js
+++ Number_Guessing_Game/ng.js
@@ -6,32 +6,28 @@ document
   .getElementById("guess-form")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-    document.getElementById("try").innerText =
-      "You have tried " + (score += 1) + " times.";
     let num2 = document.getElementById("guess").value;
     if (guesses.includes(num2)) {
       document.getElementById("message").innerText =
-        "You already guessed that number";
-    } else {
-      guesses.push(num2);
-      document.getElementById("guessesMade").innerText =
-            "Your guesses: " + guesses.join(", ");
-    if (num2 == num) {
-                document.getElementById("message").innerText =
-                  "Congratulations! You guessed the number";
-              }
-      else if (num2 > num) {
-        document.getElementById("message").innerText = "Too high, try again";
-      } else if (num2 < num) {
-        document.getElementById("message").innerText = "Too low, try again";
-      }  else {
-        score -= 1;
-        document.getElementById("message").innerText = "Invalid input";
-      }
+        "You have already guessed that number";
+    } else if (num2 > num) {
+      document.getElementById("message").innerText = "Too high, try again";
+    } else if (num2 < num) {
+      document.getElementById("message").innerText = "Too low, try again";
+    } else if (num2 == num) {
+      document.getElementById("message").innerText =
+        "Congratulations! You guessed the number";
     }
+    guesses.push(num2);
+    document.getElementById("guessesMade").innerText =
+      "Already guessed: " + guesses.join(", ");
+    score += 1;
+    document.getElementById("try").innerText =
+      "You have tried " + score + " times.";
   });
 
-document.getElementById("revealNumber").addEventListener("click", function () {
-  alert("The secret number is " + num);
-  score += 10;
+  document.getElementById('revealNumber').addEventListener('click', function() {
+    alert('The secret number is ' + num);
+    score += 10;
+    document.getElementById("try").innerText = "You have tried " + score + " times.";
 });
