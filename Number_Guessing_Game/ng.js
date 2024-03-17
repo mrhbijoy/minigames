2ad0ef968900cd18bdb9e4c753833bbb134f44f9
Hasan Bijoy <mrhbijoy@gmail.com>
2024-03-17 14:47:06 +0600

    new

--- Number_Guessing_Game/ng.js
+++ Number_Guessing_Game/ng.js
@@ -15,15 +15,16 @@ document
     } else {
       guesses.push(num2);
       document.getElementById("guessesMade").innerText =
-        "Your guesses: " + guesses.join(", ");
-      if (num2 > num) {
+            "Your guesses: " + guesses.join(", ");
+    if (num2 == num) {
+                document.getElementById("message").innerText =
+                  "Congratulations! You guessed the number";
+              }
+      else if (num2 > num) {
         document.getElementById("message").innerText = "Too high, try again";
       } else if (num2 < num) {
         document.getElementById("message").innerText = "Too low, try again";
-      } else if (num2 == num) {
-        document.getElementById("message").innerText =
-          "Congratulations! You guessed the number";
-      } else {
+      }  else {
         score -= 1;
         document.getElementById("message").innerText = "Invalid input";
       }
